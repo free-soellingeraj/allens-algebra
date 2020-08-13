a50444cabc12c60f66f344891363a8f18d9c5e5e
Aaron Soellinger <asoellin@gmail.com>
2020-08-13 11:01:26 -0400

    updated attributes

--- allens_algebra/__init__.py
+++ allens_algebra/__init__.py
@@ -1,1 +1,19 @@
-from _version import __version__
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+import sys
+
+from pbr.version import VersionInfo
+
+_v = VersionInfo('allens_algebra').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
+
+__author__ = "Aaron Soellinger"
+__copyright__ = "Copyright 2020-, Aaron Soellinger"
+__email__ = "asoellin@gmail.com"
+__status__ = "Alpha"
+__all__ = ('allens_algebra, __version__')
+
+if sys.hexversion < 0x02050000:
+    sys.exit("Python 2.5 or newer is required by tendo module.")
